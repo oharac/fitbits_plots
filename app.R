4dd39689a92f0c702c8f4daf3ac8cf568a889708
Casey O'Hara <casey.c.ohara@gmail.com>
2021-08-31 14:15:20 -0700

    updated so values are in an editable data table

--- app.R
+++ app.R
@@ -11,119 +11,110 @@ library(shiny)
 library(tidyverse)
 library(plotly)
 library(cowplot)
+library(DT)
 
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
-    titlePanel("FITBITS examples"),
+    titlePanel("FITBITS Prioritization"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
             includeMarkdown('instructions.Md'),
-            h4('Species A'),
-            textInput("sppA_name", label = NULL,
-                      value = 'Triceratops'),
-            textInput("sppA_coords", label = NULL,
-                      value = '(0.25, 0.50, 0.75)'),
-            h4('Species B'),
-            textInput("sppB_name", label = NULL,
-                      value = '(name)'),
-            textInput("sppB_coords", label = NULL,
-                      value = '(U, M, NCP)'),
-            h4('Species C'),
-            textInput("sppC_name", label = NULL,
-                      value = '(name)'),
-            textInput("sppC_coords", label = NULL,
-                      value = '(U, M, NCP)'), 
-            h4('Species D'),
-            textInput("sppD_name", label = NULL,
-                      value = '(name)'),
-            textInput("sppD_coords", label = NULL,
-                      value = '(U, M, NCP)'),
-            h4('Species E'),
-            textInput("sppE_name", label = NULL,
-                      value = '(name)'),
-            textInput("sppE_coords", label = NULL,
-                      value = '(U, M, NCP)')
+            actionButton("go", label = "Plot data"),
+            hr(),
+            DTOutput("dt_out")
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
+            # plotOutput("dt_plot")
             plotOutput("ncp2dPlot"),
             plotlyOutput("ncp3dPlotly")
         )
     )
 )
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    spp_df <- reactive({
-        message('in spp_df()')
-        input_df <- data.frame(spp_id = c(input$sppA_name,
-                                          input$sppB_name,
-                                          input$sppC_name,
-                                          input$sppD_name,
-                                          input$sppE_name),
-                               coords = c(input$sppA_coords,
-                                          input$sppB_coords,
-                                          input$sppC_coords,
-                                          input$sppD_coords,
-                                          input$sppE_coords)) %>%
-            rowwise() %>%
-            mutate(coord_extract = str_split(coords, ','),
-                   u   = coord_extract[[1]], 
-                   m   = ifelse(length(coord_extract) > 1, coord_extract[[2]], NA),
-                   ncp = ifelse(length(coord_extract) > 2, coord_extract[[3]], NA)) %>%
-            mutate(u   = as.numeric(str_extract(u, '[0-9\\.]+')),
-                   m   = as.numeric(str_extract(m, '[0-9\\.]+')),
-                   ncp = as.numeric(str_extract(ncp, '[0-9\\.]+'))) %>%
-            ### constrain to 0-1
-            mutate(u   = max(min(u, 1), 0),
-                   m   = max(min(m, 1), 0),
-                   ncp = max(min(ncp, 1), 0)) %>%
-            ungroup() %>%
-            select(-coord_extract) %>%
-            drop_na()
-        print(knitr::kable(input_df))
-        return(input_df)
+    ### initialize a blank dataframe
+    fb_df <- reactiveValues(
+        data = { 
+            # message('inside reactiveValues')
+            data.frame(
+                bit_name = c('e.g.: Bee richness', rep('(name/desc of bit)', 9)),
+                U        = c(.3,                   rep(NA_real_, 9)),
+                M        = c(.5,                   rep(NA_real_, 9)),
+                NCP      = c(.8,                   rep(NA_real_, 9)))
     })
     
+    output$dt_out <- renderDT({
+        message('inside renderDT')
+        DT::datatable(fb_df$data, editable = TRUE)
+    })
+    
+    ### when there is any edit to a cell, write that edit to the initial dataframe
+    ### check to make sure it's positive, if not convert
+    observeEvent(input$dt_out_cell_edit, {
+        message('inside observeEvent')
+        ### get values
+        info <- input$dt_out_cell_edit
+        i <- as.numeric(info$row)
+        j <- as.numeric(info$col)
+        k <- ifelse(j > 1, 
+                    max(min(as.numeric(info$value), 1), 0),
+                    as.character(info$value))
+
+        #write values to reactive
+        fb_df$data[i, j] <- k
+    })
+    
+    #render plot
     output$ncp2dPlot <- renderPlot({
 
-        plot_df <- spp_df()
+        message('inside renderPlot')
+        ###  require the input button to be non-0 (ie: don't load the plot when the app first loads)
+        req(input$go + 1)
+        tmp_df <- isolate(fb_df$data) %>%  ### don't react to any changes in the data
+            drop_na()
+        print(knitr::kable(tmp_df))
         
-        p1 <- ggplot(plot_df, aes(x = u, y = ncp, color = spp_id, shape = spp_id)) +
+        p1 <- ggplot(tmp_df, aes(x = U, y = NCP, color = bit_name, shape = bit_name)) +
             theme_classic() +
             theme(legend.title = element_blank()) +
             scale_x_continuous(breaks = seq(0, 1, .2), limits = c(0, 1)) +
             scale_y_continuous(breaks = seq(0, 1, .2), limits = c(0, 1)) +
-            labs(x = 'Uniqueness/irreplaceability', 
+            labs(x = 'Uniqueness/irreplaceability',
                  y = 'Magn. of global effect on NCP') +
             geom_point(size = 3)
-        p2 <- ggplot(plot_df, aes(x = m, y = ncp, color = spp_id, shape = spp_id)) +
+        p2 <- ggplot(tmp_df, aes(x = M, y = NCP, color = bit_name, shape = bit_name)) +
             theme_classic() +
             scale_x_continuous(breaks = seq(0, 1, .2), limits = c(0, 1)) +
             scale_y_continuous(breaks = seq(0, 1, .2), limits = c(0, 1)) +
             labs(x = 'Multifunctionality', y = 'Magn. of global effect on NCP') +
             geom_point(size = 3, show.legend = FALSE)
-        
+
         ggdraw() +
-            draw_plot(p1 + theme(legend.position = 'none'), 
+            draw_plot(p1 + theme(legend.position = 'none'),
                       x = 0.0, y = 0.0, height = 1, width = 0.4) +
             draw_plot(p2, x = 0.4, y = 0.0, height = 1, width = 0.4) +
             draw_plot(get_legend(p1), x = 0.8, y = 0, height = 1, width = 0.2)
     })
+    
     output$ncp3dPlotly <- renderPlotly({
-        plotly_df <- spp_df()
+        message('inside renderPlotly')
+        ###  require the input button to be non-0 (ie: don't load the plot when the app first loads)
+        req(input$go + 1)
+        tmp_df <- isolate(fb_df$data) %>%  ### don't react to any changes in the data
+            drop_na()
         
-        plot_ly(x = plotly_df$u, 
-                y = plotly_df$m, 
-                z = plotly_df$ncp, 
-                type = "scatter3d", mode = "markers", name = plotly_df$spp_id)
+        plot_ly(x = tmp_df$U,
+                y = tmp_df$M,
+                z = tmp_df$NCP,
+                type = "scatter3d", mode = "markers", name = tmp_df$bit_name)
     })
 }
 
